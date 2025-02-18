c68f272a0bbf653cfaf441801e6abf0a6b3a0075
HarryTortus <mickeywilliamsonart@gmail.com>
2025-02-18 14:41:45 -0500

    Update script.js bugfix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let shapes = [];
 let gravitySlider, lSystemSlider, collisionSlider, sizeSlider;
 let sliderContainer, buttonContainer, controlContainer;
-let versionNumber = "0.22"; // Updated version number
+let versionNumber = "0.22b"; // Updated version number
 let selectedShape = 'circle'; 
 let motionActive = false; 
 const MAX_SHAPES = 100;
@@ -150,12 +150,15 @@ function windowResized() {
 function mousePressed() {
     // Prevent shapes from being placed on the bottom bar (adjusted click detection)
     if (mouseY < height - 50 && mouseY > height - controlContainer.elt.clientHeight) {
-        if (shapes.length >= MAX_SHAPES) {
-            shapes.shift();
-        }
-        let s = new Shape(mouseX, mouseY, selectedShape);
-        shapes.push(s);
+        return; // Prevent interaction with the bottom bar
     }
+    
+    if (shapes.length >= MAX_SHAPES) {
+        shapes.shift(); // Remove the first shape if max limit is exceeded
+    }
+    
+    let s = new Shape(mouseX, mouseY, selectedShape);
+    shapes.push(s);
 }
 
 // Shape class
