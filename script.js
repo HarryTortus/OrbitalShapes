2d9e8617d1846e4aa6552bcaa1d9bfd86d3b3bce
HarryTortus <mickeywilliamsonart@gmail.com>
2025-02-17 22:24:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let shapes = [];
 let sliderA, sliderB, sliderC;
+let sliderContainer;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0);
     
     // Create sliders at the bottom of the screen with a gray border
-    let sliderContainer = createDiv('').style('position', 'absolute')
+    sliderContainer = createDiv('').style('position', 'absolute')
                                      .style('bottom', '10px')
                                      .style('left', '50%')
                                      .style('transform', 'translateX(-50%)')
@@ -29,8 +30,11 @@ function draw() {
 }
 
 function mousePressed() {
-    let s = new Shape(width / 2, height / 2); // Start movement in the middle
-    shapes.push(s);
+    // Prevent shapes from being added when clicking on sliders
+    if (mouseY < height - 50) {
+        let s = new Shape(width / 2, height / 2); // Start movement in the middle
+        shapes.push(s);
+    }
 }
 
 class Shape {
