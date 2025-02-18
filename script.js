58dcbbe830ca4dffa39c8abff1568f8c95662f7d
HarryTortus <mickeywilliamsonart@gmail.com>
2025-02-17 23:02:12 -0500

    Update script.js framerate (blank screen?)

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let shapes = [];
 let sliderA, sliderB, sliderC, sizeSlider;
 let sliderContainer, buttonContainer;
-let versionNumber = "0.03"; // Change this for version updates
+let versionNumber = "0.04"; // Change this for version updates
 let selectedShape = 'circle'; // Default shape
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    frameRate(30);
+    frameRate(60); // Increased frame rate for smoother motion
     background(0);
     
     // Remove default margins and padding to prevent scrollbars
@@ -78,8 +78,8 @@ class Shape {
         let a = sliderA.value();
         let b = sliderB.value();
         let c = sliderC.value();
-        this.x += sin(frameCount * 0.01 * a) * b;
-        this.y += cos(frameCount * 0.01 * b) * c;
+        this.x += sin(frameCount * 0.1 * a) * b; // Increased motion speed
+        this.y += cos(frameCount * 0.1 * b) * c;
     }
     display() {
         fill(this.color);
@@ -97,34 +97,3 @@ class Shape {
     }
 }
 
-
-class Shape {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.size = random(20, 50);
-        this.color = color(random(255), random(255), random(255));
-        this.type = random(['circle', 'square', 'triangle']); // Random shape type
-    }
-    update() {
-        let a = sliderA.value();
-        let b = sliderB.value();
-        let c = sliderC.value();
-        this.x += sin(frameCount * 0.01 * a) * b;
-        this.y += cos(frameCount * 0.01 * b) * c;
-    }
-    display() {
-        fill(this.color);
-        noStroke();
-        if (this.type === 'circle') {
-            ellipse(this.x, this.y, this.size);
-        } else if (this.type === 'square') {
-            rectMode(CENTER);
-            rect(this.x, this.y, this.size, this.size);
-        } else if (this.type === 'triangle') {
-            triangle(this.x, this.y - this.size / 2, 
-                     this.x - this.size / 2, this.y + this.size / 2, 
-                     this.x + this.size / 2, this.y + this.size / 2);
-        }
-    }
-}
